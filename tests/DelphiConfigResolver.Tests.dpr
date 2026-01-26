9d476664454b84ec685f158024ecf2b9e0d9636b
Pawel Piotrowski <pawel@maxlogic.eu>
2026-01-26 14:56:02 +0100

    Add DUnitX test project scaffold

---
+++ tests/DelphiConfigResolver.Tests.dpr
@@ -0,0 +1,40 @@
+program DelphiConfigResolverTests;
+
+{$APPTYPE CONSOLE}
+{$STRONGLINKTYPES ON}
+
+uses
+  System.SysUtils,
+  DUnitX.TestFramework,
+  DUnitX.Loggers.Console,
+  Tests.Support in 'units\\tests.support.pas',
+  Tests.Build in 'units\\tests.build.pas';
+
+var
+  Runner: ITestRunner;
+  Results: IRunResults;
+  Logger: ITestLogger;
+begin
+  ReportMemoryLeaksOnShutdown := True;
+  try
+    TDUnitX.CheckCommandLine;
+    Runner := TDUnitX.CreateRunner;
+    Runner.UseRTTI := True;
+    Runner.FailsOnNoAsserts := False;
+
+    Logger := TDUnitXConsoleLogger.Create(True);
+    Runner.AddLogger(Logger);
+
+    Results := Runner.Execute;
+    if not Results.AllPassed then
+      System.ExitCode := 1
+    else
+      System.ExitCode := 0;
+  except
+    on E: Exception do
+    begin
+      Writeln(E.ClassName, ': ', E.Message);
+      System.ExitCode := 1;
+    end;
+  end;
+end.
