0ab6bfc6eac6d7c61130adbe7ce3307b00d808d7
Pawel Piotrowski <pawel@maxlogic.eu>
2026-02-27 18:43:25 +0100

    test: harden resolver build helper for locked output

--- tests/units/test.build.pas
+++ tests/units/test.build.pas
@@ -4,6 +4,8 @@ interface
 
 uses
   DUnitX.TestFramework,
+  System.Classes,
+  System.IOUtils,
   System.SysUtils,
   Test.Support;
 
@@ -13,6 +15,8 @@ type
   public
     [Test]
     procedure BuildResolverExe;
+    [Test]
+    procedure BuildResolverExeIgnoresLockedDefaultOutput;
   end;
 
 implementation
@@ -21,6 +25,28 @@ procedure TBuildTests.BuildResolverExe;
 begin
   EnsureResolverBuilt;
   Assert.IsTrue(FileExists(ResolverExePath), 'Resolver exe not found: ' + ResolverExePath);
+end;
+
+procedure TBuildTests.BuildResolverExeIgnoresLockedDefaultOutput;
+var
+  lBinExePath: string;
+  lLockFile: TFileStream;
+begin
+  lBinExePath := TPath.Combine(RepoRoot, 'bin\DelphiAIKit.exe');
+  ResetResolverBuildCache;
+  TDirectory.CreateDirectory(TPath.GetDirectoryName(lBinExePath));
+  if not FileExists(lBinExePath) then
+    TFile.WriteAllBytes(lBinExePath, [Byte($4D), Byte($5A)]);
+
+  lLockFile := TFileStream.Create(lBinExePath, fmOpenReadWrite or fmShareDenyWrite);
+  try
+    EnsureResolverBuilt;
+    Assert.IsTrue(FileExists(ResolverExePath), 'Expected resolver build to succeed even when default bin output is locked.');
+    Assert.IsFalse(SameText(TPath.GetFullPath(ResolverExePath), TPath.GetFullPath(lBinExePath)),
+      'Expected resolver build helper to use isolated output instead of bin\DelphiAIKit.exe.');
+  finally
+    lLockFile.Free;
+  end;
 end;
 
 initialization
